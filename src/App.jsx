1dfe5c534208960c16358612c81f597f1fde4fa9
romanku <roma.kulakov@gmail.com>
2017-12-09 17:57:01 +0200

    init data loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import EventList from './event/EventList';
 
-const App = () => (
-	<EventList/>
-	);
+import {getFixtures} from './api/ApiService';
 
+class App extends React.Component {
+	constructor(props){
+		super();
+		this.state = {events: []}
+	}
+
+	componentDidMount() {
+    	getFixtures((data) => {
+			this.setState({events: data});
+		});
+  	}
+
+	render(){
+		return <EventList events={this.state.events}/>
+	}
+}
 export default App;
